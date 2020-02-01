b9d75b11569267ff863b9d7d57bc6d89f927d05d
PhilippThoelke <p.thoelke@gmx.de>
2020-02-01 19:45:07 +0100

    Add file containing various constants used by the bot

---
+++ constants.py
@@ -0,0 +1,5 @@
+START_MENU_MESSAGE = 'Wähle aus worüber du mehr wissen möchtest:'
+INTERNAL_ERROR_MESSAGE = 'Ein Fehler ist aufgetreten...'
+
+SNEAK_CALLBACK_DATA = 'SNEAK'
+SNEAK_BUTTON_TEXT = 'Sneak Preview'
